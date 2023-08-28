d8558b779e29bc0dbcc5fb29839136d1e3d678e6
stayhomewelldone <0973013@hr.nl>
2023-08-28 17:47:58 +0200

    Changed size and place

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,30 +52,30 @@ const Navbar = () => {
         className={
           !nav
             ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center  classname='py-6'"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center "
         }
       >
-        <li classname="py-6 text-4xl">
+        <li className="py-6 text-4xl">
           <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li classname="py-6 text-4xl">
+        <li className="py-6 text-4xl">
           <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li classname="py-6 text-4xl">
+        <li className="py-6 text-4xl">
           <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li classname="py-6 text-4xl">
+        <li className="py-6 text-4xl">
           <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li classname="py-6 text-4xl">
+        <li className="py-6 text-4xl">
           <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
           </Link>
